f5c8bc14a51c685ad58ccc2963c43d4fd8377936
darrylbckhm <darrylbckhm@gmail.com>
2017-03-12 21:22:50 -0700

    Trying to use continuation token in s3 bucket list, but value drops at end of loop. Needs fix.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ extern crate rusoto;
 use std::default::Default;
 
 use rusoto::{DefaultCredentialsProvider, Region};
-use rusoto::s3::ListObjectsRequest;
+use rusoto::s3::ListObjectsV2Request;
 use rusoto::s3::S3Client;
 use rusoto::default_tls_client;
 
@@ -17,20 +17,30 @@ fn list_bucket_objects(){
         Region::UsEast1
     );
 
-    let mut list_request = ListObjectsRequest::default();
+    let mut list_request = ListObjectsV2Request::default();
     list_request.bucket = "dabeckham.first.bucket".to_string();
     list_request.prefix = Some("list1000".to_string());
-    let result = bare_s3.list_objects(&list_request).unwrap();
+    let mut continuation_token = "";
 
-    //loop {
+    loop {
+        if continuation_token != ""{
+            list_request.continuation_token = Some(continuation_token.to_string());
+        }
+
+        let result = bare_s3.list_objects_v2(&list_request).unwrap();
+
+        //I think result goes out of scope after the first iteration of the loop
+        //result needs to update on each iteration
         for obj in &result.contents.expect("Contents not found!"){
             println!("{:?}", obj.key.clone().unwrap());
         }
 
-        //if (&result).is_truncated.is_none(){
-        //    break;
-        //}
-    //}
+        if (result).is_truncated.is_none(){
+            break;
+        }
+
+        continuation_token = &result.continuation_token.expect("No token!").as_ref();
+    }
 }
 
 fn main() {
